2434b19afcc1a05ca80b16853cfc3585b459b1e8
Joshua <ejembijoshman@gmail.com>
2023-02-07 12:03:45 +0100

    amend hero section image view height

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div className="order-2">
+        <div className="flex order-2">
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -53,10 +53,39 @@ const Navbar = () => {
           >
             Membership
           </button>
+
+          <button
+            type="button"
+            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent1"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="sr-only">Open main menu</span>
+            <svg
+              class="w-6 h-6"
+              aria-hidden="true"
+              focusable="false"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
 
-        <div className="navLinks md:order-1 w-full md:block md:w-auto">
-          <ul className="hidden lg:flex flex-col p-4 mt-4 justify-between md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <div
+          className="collapse navbar-collapse navLinks md:order-1 w-full md:block md:w-auto"
+          id="navbarSupportedContent1"
+        >
+          <ul className="flex flex-col p-4 mt-4 justify-between md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             {navLinks.map((navItems, index) => {
               return (
                 <li
